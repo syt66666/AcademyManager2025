ead0f6c3d79c70df4e8345c126f1bc011e2563ea
YiFei <2514265069@qq.com>
2024-12-25 13:36:18 +0800

    Update index.vue
    修改

--- ruoyi-ui/src/views/system/role/index.vue
+++ ruoyi-ui/src/views/system/role/index.vue
@@ -205,7 +205,7 @@ export default {
         // 设置上传的请求头部
         headers: { Authorization: "Bearer " + getToken() },
         // 上传的地址
-        url: process.env.VUE_APP_BASE_API + "/system/user/importData"
+        url: process.env.VUE_APP_BASE_API + "/student/excel/upload"
       },
       // 遮罩层
       loading: true,
