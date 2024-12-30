a4b1a1222c6cfade625b739b6c3fa649246b261b
Ro1eee <Ro1eee@noreply.gitcode.com>
2024-12-30 17:11:32 +0800

    报告审核界面

--- ruoyi-ui/src/api/student/letcure.js
+++ ruoyi-ui/src/api/student/letcure.js
@@ -2,9 +2,12 @@ import request from '@/utils/request'
 
 // 查询学生管理列表
 export function upLoad(query) {
-    return request({
-        url: '/Lecture/insert',
-        method: 'post',
-        data: query
-    })
+  return request({
+    url: '/Lecture/insert',
+    method: 'post',
+    data: query,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
 }
--- ruoyi-ui/src/views/ReportManagement/index.vue
+++ ruoyi-ui/src/views/ReportManagement/index.vue
@@ -68,29 +68,13 @@
             <el-form-item>
               <div style="display: flex; align-items: center;">
                 <span class="form-item-label" style="font-size: 16px;width:70px;height: auto">报告心得体会上传</span>
-                <el-upload
-                  class="upload-demo"
-                  drag
-                  action="https://jsonplaceholder.typicode.com/posts/"
-                  style="margin-left: 10px"
-                  multiple>
-                  <i class="el-icon-upload"></i>
-                  <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-                </el-upload>
+                <input type="file" @change="onFileChange" accept="*/*" />
               </div>
             </el-form-item>
             <el-form-item>
               <div style="display: flex; align-items: center;">
                 <span class="form-item-label" style="font-size: 16px;width:70px">图片上传</span>
-                <el-upload
-                  class="upload-demo"
-                  drag
-                  action="https://jsonplaceholder.typicode.com/posts/"
-                  style="margin-left: 10px"
-                  multiple>
-                  <i class="el-icon-upload"></i>
-                  <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-                </el-upload>
+                <input type="file" @change="onImageChange" accept="image/*" />
               </div>
             </el-form-item>
             <el-form-item>
@@ -116,14 +100,14 @@ export default {
       newCardInfo: '',
       selectedFile: null,
       uploadMessage: null,
+      reportFeeling: null,
+      picture: null,
       formData: {
         title: '',
         reporter: '',
         reportDate: '',
         reportContent: '',
         link: '',
-        reportFeeling: null,
-        picture: null
       },
     };
   },
@@ -134,49 +118,35 @@ export default {
     closeCard() {
       this.showSecondCard = false;
     },
-    handleExperienceUploadSuccess(response, file, fileList) {
-      // 处理报告心得体会上传成功后的逻辑
-      console.log('报告心得体会上传成功', response);
+    onFileChange(e) {
+      // 当用户选择文件时，更新file变量
+      this.reportFeeling = e.target.files[0];
     },
-    beforeExperienceUpload(file) {
-      // 报告心得体会上传前的检查逻辑，例如文件大小、类型检查
-      const isLt2M = file.size / 1024 / 1024 < 2;
-      if (!isLt2M) {
-        this.$message.error('文件大小不能超过 2MB!');
-        return false;
-      }
-      this.formData.experienceFile = file;
-      return true;
-    },
-    handleImageUploadSuccess(response, file, fileList) {
-      // 处理图片上传成功后的逻辑
-      console.log('图片上传成功', response);
-    },
-    beforeImageUpload(file) {
-      // 图片上传前的检查逻辑，例如文件大小、类型检查
-      const isLt2M = file.size / 1024 / 1024 < 2;
-      if (!isLt2M) {
-        this.$message.error('文件大小不能超过 2MB!');
-        return false;
-      }
-      this.formData.imageFiles.push(file);
-      return true;
+    onImageChange(e) {
+      // 当用户选择图片时，更新images数组
+      this.picture = Array.from(e.target.files);
     },
     submitForm() {
       this.$refs.form.validate((valid) => {
         if (valid) {
           //将本地时间转为 ISO 格式的 UTC 时间
           this.formData.reportDate = new Date(`${this.formData.reportDate} UTC`).toISOString();
+          const formData = new FormData();
+          const json = JSON.stringify(this.formData);
+          formData.append('studentLectureReport', json);
+          formData.append('reportFeeling', this.reportFeeling);
+          formData.append('picture', this.picture[0]);
           // 在这里编写提交表单的逻辑，例如将表单数据发送到后端
           console.log('表单数据:', this.formData);
+          console.log('传递后端数据:', formData);
           // 可以使用 axios 或 fetch 发送请求
           // 例如：
-          upLoad(this.formData).then(response => {
-             console.log("+++++++++",response);
-           })
-          .catch(error => {
-             console.error(error);
-           });
+          upLoad(formData).then(response => {
+            console.log("+++++++++", response);
+          })
+            .catch(error => {
+              console.error(error);
+            });
         } else {
           this.$message.error('请填写完整表单信息');
         }
