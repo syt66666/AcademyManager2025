0e41cf6e47f436e93f8471ed008e4482880e6a40
syt66666 <2857054421@qq.com>
2025-02-24 13:43:49 +0800

    优化问卷代码

--- ruoyi-system/src/main/java/com/ruoyi/system/controller/StudentInfoController.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/controller/StudentInfoController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.HashMap;
 import java.util.Map;
 
