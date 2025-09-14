5a190452488e3930e65a8dd382c1cd59369c577e
syt66666 <2857054421@qq.com>
2025-09-14 14:03:56 +0800

    去掉导入导出功能

--- ruoyi-ui/src/views/Activity/ActivityManager.vue
+++ ruoyi-ui/src/views/Activity/ActivityManager.vue
@@ -95,20 +95,6 @@
           :disabled="multiple"
           @click="handleDelete"
         >删除</el-button>
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-upload"
-          size="mini"
-          @click="handleImport"
-        >导入</el-button>
-        <el-button
-          type="warning"
-          plain
-          icon="el-icon-download"
-          size="mini"
-          @click="handleExport"
-        >导出</el-button>
       </el-button-group>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </div>
@@ -621,37 +607,6 @@
         </div>
       </div>
     </el-dialog>
-    <!-- 用户导入对话框 -->
-    <el-dialog :title="upload.title" :visible.sync="upload.open" width="400px">
-      <el-upload
-        ref="upload"
-        :limit="1"
-        accept=".xlsx, .xls"
-        :headers="upload.headers"
-        :action="upload.url + '?updateSupport=' + upload.updateSupport"
-        :disabled="upload.isUploading"
-        :on-progress="handleFileUploadProgress"
-        :on-success="handleFileSuccess"
-        :auto-upload="false"
-        drag
-      >
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">
-          将文件拖到此处，或
-          <em>点击上传</em>
-        </div>
-        <div class="el-upload__tip" slot="tip">
-          <el-checkbox v-model="upload.updateSupport"/>
-          是否更新已经存在的用户数据
-          <el-link type="info" style="font-size:12px" @click="importTemplate">下载模板</el-link>
-        </div>
-        <div class="el-upload__tip" style="color:red" slot="tip">提示：仅允许导入"xls"或"xlsx"格式文件！</div>
-      </el-upload>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="submitFileForm">确 定</el-button>
-        <el-button @click="upload.open = false">取 消</el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
@@ -758,21 +713,6 @@ export default {
         createdAt: null,
         organizer: null,
         notes: null,
-      },
-      // 用户导入参数
-      upload: {
-        // 是否显示弹出层（用户导入）
-        open: false,
-        // 弹出层标题（用户导入）
-        title: "",
-        // 是否禁用上传
-        isUploading: false,
-        // 是否更新已经存在的用户数据
-        updateSupport: 0,
-        // 设置上传的请求头部
-        headers: {Authorization: "Bearer " + getToken()},
-        // 上传的地址
-        url: process.env.VUE_APP_BASE_API + "/system/activities/importData"
       },
       // 表单参数
       form: {},
@@ -1060,43 +1000,6 @@ export default {
       } else {
         this.form.status = '未开始';
       }
-    },
-    /** 导入模板操作 */
-    importTemplate() {
-      fetch(process.env.VUE_APP_BASE_API + '/system/activities/importTemplate', {
-        mode: 'cors', // 明确跨域模式
-        headers: new Headers({'Content-Type': 'application/json'})
-      })
-        .then(response => response.blob())
-        .then(blob => {
-          const url = window.URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.href = url;
-          a.download = '书院活动信息.xlsx'; // 指定下载文件名
-          a.click();
-          window.URL.revokeObjectURL(url);
-        });
-    },
-    /** 导入按钮操作 */
-    handleImport() {
-      this.upload.title = "用户导入";
-      this.upload.open = true;
-    },
-// 文件上传中处理
-    handleFileUploadProgress(event, file, fileList) {
-      this.upload.isUploading = true;
-    },
-// 文件上传成功处理
-    handleFileSuccess(response, file, fileList) {
-      this.upload.open = false;
-      this.upload.isUploading = false;
-      this.$refs.upload.clearFiles();
-      this.$alert(response.msg, "导入结果", {dangerouslyUseHTMLString: true});
-      this.getList();
-    },
-    // 提交上传文件
-    submitFileForm() {
-      this.$refs.upload.submit();
     },
     /** 查询活动列表 */
     getList() {
@@ -1437,12 +1340,6 @@ export default {
         this.$message.success("删除成功");
       }).catch(() => {
       });
-    },
-    /** 导出按钮操作 */
-    handleExport() {
-      this.download('system/activities/export', {
-        ...this.queryParams
-      }, `activities_${new Date().getTime()}.xlsx`)
     },
 
     /** 更新可用的活动类型列表 */
